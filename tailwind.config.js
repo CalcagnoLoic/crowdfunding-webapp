237423be923f9dc55d2d4676e8d793ab69d27d4d
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2024-02-20 13:32:45 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        keppel: "#3CB4AC",
+        genoa: "#147B74",
+        codGray: "#131313",
+        boulder: "#7A7A7A",
+        wildSand: "#F4F4F4", //bookmark-btn
+        silver: "#CBCBCB", //disabled-btn
+        gallery: "#EAEAEA", //border box
+        gulfStream: "#8BB4B3", //active-box
+        patina: "#649694", //text-modal
+      }
+    },
+  },
+  plugins: [],
+};
