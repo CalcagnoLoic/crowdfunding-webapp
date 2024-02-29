749dd6ac8a71917341a1d705f4ac43cdcaed84c2
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2024-02-29 18:37:58 +0100

    (update): updating icon component

--- src/icons/IconBookmark/index.tsx
+++ src/icons/IconBookmark/index.tsx
@@ -1,15 +1,13 @@
 type BookmarkProps = {
   clicked: boolean;
-  onClick?: () => void;
 };
 
-const Icon = ({ clicked, onClick }: BookmarkProps) => (
+const Icon = ({ clicked }: BookmarkProps) => (
   <svg
     width="56"
     height="56"
     xmlns="http://www.w3.org/2000/svg"
     className="ml-3 cursor-pointer md:ml-0"
-    onClick={onClick}
   >
     <g fill="none" fillRule="evenodd">
       <circle fill={clicked ? "#147B74" : "#2F2F2F"} cx="28" cy="28" r="28" />
